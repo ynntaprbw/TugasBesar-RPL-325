f67c2040225bb2cb88eee7e4c6e3e1102d01f256
rakaramadhani <rakaramadhani2001@gmail.com>
2024-04-15 12:18:55 +0700

    Update Migration

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('/register', function () {return view('register');}) -> name('register');
+Route::get('/', function () {
+    return view('welcome');
+});
