e7caf2a6b75411d02b20f70abfdae70f718c699b
Xyla Maharani <xylamharanii@gmail.com>
2024-04-16 15:58:44 +0700

    database update

--- database/migrations/2024_04_16_043313_create_denda.php
+++ database/migrations/2024_04_16_043313_create_denda.php
@@ -1,42 +1,34 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
-return new class extends Migration
+class CreateDendaTable extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-    public function up(): void
+    public function up()
     {
         Schema::create('denda', function (Blueprint $table) {
-            $table->id('idDenda');
+            $table->id();
             $table->unsignedBigInteger('idPeminjaman');
             $table->unsignedBigInteger('idUser');
-            // $table->integer('idAdmin');
             $table->integer('totalDenda');
             $table->dateTime('tanggalBayarDenda');
-            $table->string('statusDenda')->default('Unpaid');
+            $table->string('statusDenda');
             $table->string('metodePembayaran');
             $table->timestamps();
 
-            
             $table->foreign('idUser')->references('idUser')->on('users');
-            // $table->foreign('idAdmin')->references('idAdmin')->on('admin');
             $table->foreign('idPeminjaman')->references('idPeminjaman')->on('peminjaman');
-            
-            
-            $table->checkConstraint('statusDenda IN ("Paid", "Unpaid", "In Verify")');
         });
+
+        // Adding check constraint for statusDenda column
+        DB::statement('ALTER TABLE denda ADD CONSTRAINT chk_statusDenda CHECK (statusDenda IN ("Paid", "Unpaid", "Verifying"))');
     }
 
-    /**
-     * Reverse the migrations.
-     */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('denda');
     }
-};
+}
+
