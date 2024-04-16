ab541207d566cf7afad4bda4d980612e9efc0270
Xyla Maharani <xylamharanii@gmail.com>
2024-04-16 16:07:17 +0700

    database update

--- database/migrations/2024_04_16_043313_create_denda.php
+++ database/migrations/2024_04_16_043313_create_denda.php
@@ -1,6 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\DB; // Import the DB facade
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
