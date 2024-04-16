2649a3ff2758889841eac3ea066d6362b98efa74
Xyla Maharani <xylamharanii@gmail.com>
2024-04-16 16:01:19 +0700

    database update

--- database/migrations/2024_04_16_043313_create_denda.php
+++ database/migrations/2024_04_16_043313_create_denda.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\DB; // Import the DB facade
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
@@ -31,4 +32,3 @@ class CreateDendaTable extends Migration
         Schema::dropIfExists('denda');
     }
 }
-
