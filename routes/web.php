3d2ac13ee8fe1acc4f712bee35d642c81bc428d3
rakaramadhani <rakaramadhani2001@gmail.com>
2024-04-17 21:01:27 +0700

    database update

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('frontend.register');
 });
 Route::post('/register', function () {return view('register');}) -> name('register');
-Route::get('/', function () {
-    return view('welcome');
-});
