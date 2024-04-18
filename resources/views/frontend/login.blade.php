06b38e04c1590611b0b52a747199249fe26a9378
rakaramadhani <rakaramadhani2001@gmail.com>
2024-04-18 20:33:34 +0700

    Views Update

--- resources/views/frontend/login.blade.php
+++ resources/views/frontend/login.blade.php
@@ -7,28 +7,34 @@
     @vite('resources/css/app.css')
     <link rel="shortcut icon" href="https://img.icons8.com/material-rounded/24/user.png" type="image/x-icon">
 </head>
-<body class="bg-gray-100">
-    <div class="container mx-auto px-4">
+<body class=" bg-gradient-to-r from-blue-600 to-purple-700">
+    <div class="container mx-auto px-4 ">
         <div class="flex justify-center items-center min-h-screen">
             <div class="w-full max-w-md">
-                <form class="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4" method="POST" action="/sesi/login">
+                <form class="rounded-lg bg-white shadow-md px-8 pt-6 pb-8 mb-4" method="POST" action="/sesi/login">
                     @csrf
-                    <div class="mb-4">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+                    <h1 class="text-center text-2xl font-bold mb-6">Login Akun</h1>
+                    <div class="mb-5">
+                        <label class="block mb-2 text-sm font-medium text-black" for="email">
                             Email
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="email" type="email" value="{{ Session::get('email') }}" placeholder="Email">
+                        <input class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" value="{{ Session::get('email') }}" placeholder="Email" required>
                     </div>
                     <div class="mb-6">
-                        <label class="block text-gray-700 text-sm font-bold mb-2" for="password">
+                        <label class="block mb-2 text-sm font-medium text-black" for="password">
                             Password
                         </label>
-                        <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="password" type="password" placeholder="******************">
+                        <input class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" id="password" type="password" placeholder="******************" required>
                     </div>
-                    <div class="flex items-center justify-between">
-                        <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" name="submit" type="submit">
+                    <div class="my-8 flex items-center justify-between">
+                        <button class="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500" name="submit" type="submit">
                             Sign In
                         </button>
+                    </div>
+                    <div>
+                        <p class="text-center">
+                            Belum punya akun? <a class="text-indigo-700" href="{{ route('sesiRegister') }}">Registrasi Sekarang</a>
+                        </p>
                     </div>
                 </form>
             </div>
