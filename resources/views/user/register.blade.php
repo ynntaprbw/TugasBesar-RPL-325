dd5fefa7a744346a08b7636c8cae985d7ca0b23b
rakaramadhani <rakaramadhani2001@gmail.com>
2024-04-22 21:55:22 +0700

    Migrasi Baru

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -18,10 +18,10 @@
                 <label for="namaLengkap" class="block mb-2 text-sm font-medium text-black">
                     Nama Lengkap
                 </label>
-                <input type="text" 
-                        :value="old('namaLengkap')" 
-                        name="namaLengkap" 
-                        id="namaLengkap" 
+                <input type="text"
+                        :value="old('namaLengkap')"
+                        name="namaLengkap"
+                        id="namaLengkap"
                         class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="Nama Lengkap" required autofocus autocomplete="namaLengkap" />
             </div>
             {{-- Email --}}
@@ -29,10 +29,10 @@
                 <label for="email" class="block mb-2 text-sm font-medium text-black">
                     Email
                 </label>
-                <input type="email" 
-                        name="email" 
-                        id="email" 
-                        :value="old('email')" 
+                <input type="email"
+                        name="email"
+                        id="email"
+                        :value="old('email')"
                         class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" placeholder="name@flowbite.com" required autocomplete />
             </div>
             <div class="grid md:grid-cols-2 md:gap-6">
@@ -41,9 +41,9 @@
                     <label for="password" class="block mb-2 text-sm font-medium text-black">
                         Password
                     </label>
-                    <input type="password" 
-                            name="password" 
-                            id="password" 
+                    <input type="password"
+                            name="password"
+                            id="password"
                             class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                 </div>
                 {{-- Confirm Password --}}
@@ -51,12 +51,23 @@
                     <label for="password_confirmation" class="block mb-2 text-sm font-medium text-black">
                         Konfirmasi Password
                     </label>
-                    <input type="password" 
-                            name="password_confirmation" 
-                            id="password_confirmation" 
+                    <input type="password"
+                            name="password_confirmation"
+                            id="password_confirmation"
                             class="bg-gray-50 border border-gray-300 text-black text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5" required />
                 </div>
             </div>
+            @if($errors->has("password_confirmation"))
+                        <div class="flex items-center p-4 mb-4 text-sm text-red-800 border border-red-300 rounded-lg bg-red-50 bg-white dark:text-red-400 dark:border-red-800" role="alert">
+                            <svg class="flex-shrink-0 inline w-4 h-4 me-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                            </svg>
+                            <span class="sr-only">Info</span>
+                            <div>
+                                <span class="font-medium">Peringatan! Email atau Password yang anda masukkan salah!</span>
+                            </div>
+                        </div>
+                    @endif
             <div>
                 <p>
                     Gunakan minimal 8 karakter dengan campuran huruf, angka & simbol
