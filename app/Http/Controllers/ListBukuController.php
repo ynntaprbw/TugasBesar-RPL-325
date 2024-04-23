820b61e8d00a6a1ca65044582a60bd4cc85bc5f7
zephyrx0 <ganelajeisa@gmail.com>
2024-04-23 20:25:53 +0700

    no message

--- app/Http/Controllers/ListBukuController.php
+++ app/Http/Controllers/ListBukuController.php
@@ -2,11 +2,12 @@
 
 namespace App\Http\Controllers;
 use App\Models\Buku;
+use App\Models\Kategori;
 use Illuminate\Http\Request;
 
 class ListBukuController extends Controller
 {
-    public function index()
+    public function create()
     {
         $bukus = Buku::all();
         return view('buku.index', compact('bukus'));
