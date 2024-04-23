775768cb156782ff85486db9ee7c97280792f9a0
Xyla Maharani <xylamharanii@gmail.com>
2024-04-23 19:43:37 +0700

    buku & kategori

---
+++ app/Models/Kategori.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Buku;
+
+
+class Kategori extends Model
+{
+    protected $table = 'kategori'; // Assuming your table name is 'kategori'
+
+    protected $fillable = [
+        'namaKategori',
+        'deskripsi',
+    ];
+
+    // You can define relationships here if needed
+    // For example, if a Kategori has many Buku
+    public function bukus()
+    {
+        return $this->hasMany(Buku::class, 'idKategori');
+    }
+}
+
