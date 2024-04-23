9193002271e5cb3ebd47ea9463ddea0db9104150
Xyla Maharani <xylamharanii@gmail.com>
2024-04-23 20:25:06 +0700

    listbuku

--- app/Http/Controllers/ListBukuController.php
+++ app/Http/Controllers/ListBukuController.php
@@ -1,15 +1,30 @@
 <?php
 
 namespace App\Http\Controllers;
+
 use App\Models\Buku;
 use Illuminate\Http\Request;
 
 class ListBukuController extends Controller
 {
-    public function index()
+    public function showAll()
     {
+        // Retrieve all books
         $bukus = Buku::all();
-        return view('buku.index', compact('bukus'));
+        
+        // Return the books as JSON response
+        return response()->json($bukus);
     }
 
+    public function filterByJudul(Request $request)
+    {
+        // Retrieve the 'judul' keyword from the request
+        $keyword = $request->input('judul');
+
+        // Filter books by title using the 'judul' keyword
+        $bukus = Buku::where('judulBuku', 'like', '%' . $keyword . '%')->get();
+        
+        // Return the filtered books as JSON response
+        return response()->json($bukus);
+    }
 }
