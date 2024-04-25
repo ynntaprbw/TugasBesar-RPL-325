e921b6823c4525fa33736f519842854fe5f71885
zephyrx0 <ganelajeisa@gmail.com>
2024-04-25 13:59:48 +0700

    ulasan

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ListBukuController;
+use App\Http\Controllers\ListUlasanController;
 
 Route::middleware('guest')->group(function () {
     Route::get('/', function () {
@@ -12,7 +13,12 @@ Route::middleware('guest')->group(function () {
 Route::get('/dashboard', function () {
     return view('user.dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
+
 Route::get('/dashboard', [ListBukuController::class, 'create']);
+
+Route::get('/ulasan', [ListUlasanController::class, 'index']);
+Route::post('/ulasan/filter', [ListUlasanController::class, 'filterByRating']);
+Route::post('/ulasan/store', [ListUlasanController::class, 'store']);
 
 
 require __DIR__.'/auth.php';
