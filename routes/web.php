924df4522173eb78b0f4372ec9d4f072290a308b
Adityaseptian <adityaseptian@student.telkomuniversity.ac.id>
2024-04-25 13:21:26 +0700

    commit artikel

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
 
 Route::middleware('guest')->group(function () {
     Route::get('/', function () {
@@ -14,3 +15,11 @@ Route::get('/dashboard', function () {
 
 require __DIR__.'/auth.php';
 
+ 
+Route::get('/artikel',[PostController::class,'index']);
+Route::get('create',[PostController::class,'create']);
+Route::post('post',[PostController::class,'store']);
+Route::get('show/{id}',[PostController::class,'show']);
+Route::get('edit/{id}',[PostController::class,'edit']);
+Route::post('update/{id}',[PostController::class,'update']);
+Route::get('delete/{id}',[PostController::class,'destroy']);
