d0b9108f0bca4d2122045cf70df018a1f710628e
zephyrx0 <ganelajeisa@gmail.com>
2024-04-27 18:54:40 +0700

    artikel

--- resources/views/user/artikel.blade.php
+++ resources/views/user/artikel.blade.php
@@ -1,7 +1,7 @@
 @extends('user.dashboard')
 @section('content')
 <div>
-    @foreach($artikels as $artikel)
+    @foreach($articles as $artikel)
     <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
