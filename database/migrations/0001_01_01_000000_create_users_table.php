65fe4b19bde16695af851d98006a389550b0d6f1
Xyla Maharani <xylamharanii@gmail.com>
2024-05-13 16:54:05 +0700

    db revision

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->uuid('idUser')->primary();
             $table->string('email')->unique();
-            $table->string('namaLengkap');
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
