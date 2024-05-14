05e062f753dceffb96573a9658e76a6cf58b194b
rakaramadhani <rakaramadhani2001@gmail.com>
2024-05-14 09:12:55 +0700

    Ulasan Artikel fix

--- resources/views/user/ulasan.blade.php
+++ resources/views/user/ulasan.blade.php
@@ -3,7 +3,7 @@
 <div class="grid grid-cols-4 gap-4">
     @foreach($ulasans as $ulasan)
     <div class="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-white dark:border-gray-700">
-        <p>User: <span class="text-blue-700 font-bold">{{$ulasan->nama}}</span></p>
+        <p>User: <span class="text-blue-700 font-bold">{{$ulasan->name}}</span></p>
         <a href="#">
             <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-black">{{ $ulasan->komentar }}</h5>
         </a>
