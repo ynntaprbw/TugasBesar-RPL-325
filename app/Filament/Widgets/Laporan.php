8771fb3825889f26bd690da12d5ffba75c6cfd8c
Adityaseptian <adityaseptian@student.telkomuniversity.ac.id>
2024-06-10 08:23:13 +0700

    wigdets dan beberapa perbaikan

---
+++ app/Filament/Widgets/Laporan.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use App\Models\Pembelian;
+use App\Models\Peminjaman;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Contracts\View\View;
+
+class Laporan extends BaseWidget
+{
+    protected function getStats(): array
+    {
+        // Trend data for Pembelian
+        $pembelianData = Trend::model(Pembelian::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        // Trend data for Peminjaman
+        $peminjamanData = Trend::model(Peminjaman::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        // Return stats
+        return [
+            Stat::make('Pembelian', Pembelian::query()->count())
+                ->description('Jumlah Pembelian')
+                ->chart($pembelianData->map(fn (TrendValue $value) => $value->aggregate)->toArray())
+                ->color('success'),
+            Stat::make('Peminjaman', Peminjaman::query()->count())
+                ->description('Jumlah Peminjaman')
+                ->chart($peminjamanData->map(fn (TrendValue $value) => $value->aggregate)->toArray())
+                ->color('success'),
+        ];
+    }
+
+
+
+    protected function getData(): array
+    {
+        $pembelianData = Trend::model(Pembelian::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        $peminjamanData = Trend::model(Peminjaman::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Pembelian',
+                    'data' => $pembelianData->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+                [
+                    'label' => 'Peminjaman',
+                    'data' => $peminjamanData->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $peminjamanData->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+}
---
+++ app/Filament/Widgets/LaporanChart.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Filament\Widgets;
+
+use Filament\Widgets\ChartWidget;
+use App\Models\Pembelian;
+use App\Models\Peminjaman;
+use Filament\Widgets\StatsOverviewWidget as BaseWidget;
+use Filament\Widgets\StatsOverviewWidget\Stat;
+use Flowframe\Trend\Trend;
+use Flowframe\Trend\TrendValue;
+use Illuminate\Contracts\View\View;
+
+class LaporanChart extends ChartWidget
+{
+    protected static ?string $heading = 'Chart';
+
+    protected function getData(): array
+    {
+            $pembelianData = Trend::model(Pembelian::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        $peminjamanData = Trend::model(Peminjaman::class)
+            ->between(
+                start: now()->startOfYear(),
+                end: now()->endOfYear(),
+            )
+            ->perMonth()
+            ->count();
+
+        return [
+            'datasets' => [
+                [
+                    'label' => 'Pembelian',
+                    'data' => $pembelianData->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+                [
+                    'label' => 'Peminjaman',
+                    'data' => $peminjamanData->map(fn (TrendValue $value) => $value->aggregate),
+                ],
+            ],
+            'labels' => $peminjamanData->map(fn (TrendValue $value) => $value->date),
+        ];
+    }
+
+    protected function getType(): string
+    {
+        return 'bar';
+    }
+}
