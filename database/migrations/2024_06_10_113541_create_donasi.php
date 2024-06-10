db2a08a48c40bbd41be1d8a03da35d14d21de295
Adityaseptian <adityaseptian@student.telkomuniversity.ac.id>
2024-06-10 20:28:24 +0700

    no message

--- database/migrations/2024_06_10_113541_create_donasi.php
+++ database/migrations/2024_06_10_113541_create_donasi.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('donasi_buku', function (Blueprint $table) {
+        Schema::create('donasi', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('donasi_buku');
+        Schema::dropIfExists('donasi');
     }
 };
