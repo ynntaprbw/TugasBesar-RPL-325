a324a0b6cb0e9260e4bfb4b46bd651eb29995d8a
Adityaseptian <adityaseptian@student.telkomuniversity.ac.id>
2024-06-10 18:30:44 +0700

    add donasi table

--- database/migrations/2024_06_10_111600_create_donasi.php
+++ database/migrations/2024_06_10_111600_create_donasi.php
@@ -11,7 +11,7 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('sumbangans', function (Blueprint $table) {
+        Schema::create('donasi_buku', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
         });
@@ -22,6 +22,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('sumbangans');
+        Schema::dropIfExists('donasi_buku');
     }
 };
