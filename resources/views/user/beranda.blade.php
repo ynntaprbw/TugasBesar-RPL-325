646487ca70e9e1ca1c84d65b744f7e0dba09bd64
rakaramadhani <rakaramadhani2001@gmail.com>
2024-06-11 09:41:24 +0700

    Presentasi Gass

--- resources/views/user/beranda.blade.php
+++ resources/views/user/beranda.blade.php
@@ -21,25 +21,18 @@
     {{-- Menampilkan data dari tabel buku --}}
     <div class="grid grid-cols-4 gap-4">
         @foreach($bukus as $buku)
-
         <div class="w-full max-w-sm bg-white border border-gray-200 rounded-xl shadow dark:bg-white dark:border-gray-700">
             <!-- Ketika gambar bisa dimuat maka akan memunculkan foto Sampul -->
-            <a href="{{ route('detailBuku', ['idBuku' => $buku->idBuku]) }}" style="background-image: url('{{ Storage::url($buku->fotoSampul) }}'); background-size: cover; background-position: center; display: block; height: 200px;">
+            <a class="w-full pt-[150%] bg-gray-200 rounded-lg overflow-hidden mb-4" href="{{ route('detailBuku', ['idBuku' => $buku->idBuku]) }}" style="background-image: url('{{ Storage::url($buku->fotoSampul) }}'); background-size: cover; background-position: center; display: block; height: 200px;">
             </a>
             <div class="px-5 pb-5">
                 <a href="{{ route('detailBuku', ['idBuku' => $buku->idBuku]) }}">
                     <h5 class="text-xl font-semibold tracking-tight text-gray-900 dark:text-gray-900">{{ $buku->judulBuku }}</h5>
                 </a>
                 <p class="text-gray-900">Penulis: {{ $buku->namaPenulis }}</p>
-                <div class="flex items-center my-2">
-                    <svg class="w-4 h-4 text-yellow-300 mr-1" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 20">
-                        <path d="M20.924 7.625a1.523 1.523 0 0 0-1.238-1.044l-5.051-.734-2.259-4.577a1.534 1.534 0 0 0-2.752 0L7.365 5.847l-5.051.734A1.535 1.535 0 0 0 1.463 9.2l3.656 3.563-.863 5.031a1.532 1.532 0 0 0 2.226 1.616L11 17.033l4.518 2.375a1.534 1.534 0 0 0 2.226-1.617l-.863-5.03L20.537 9.2a1.523 1.523 0 0 0 .387-1.575Z"/>
-                    </svg>
-                    <span class="w-1 h-1 mx-1.5 bg-gray-500 rounded-full dark:bg-gray-400">{{ $buku->rating }}</span>
-                </div>
                 <p></p>
                 <div class="flex items-center justify-between mt-3">
-                    <span class="text-2xl font-bold text-blue-800 dark:text-blue-800">Rp. {{ $buku->harga }}</span><div class=" ">
+                    <span class="text-xl font-bold text-blue-800 dark:text-blue-800">Rp. {{ $buku->harga }}</span><div class=" ">
                     </div>
                     <form action="{{ route('keranjang.store', ['idBuku' => $buku->idBuku]) }}" method="POST" style="display: inline;">
                         @csrf
@@ -48,8 +41,6 @@
                 </div>
             </div>
         </div>
-
-
         @endforeach
     </div>
 </div>
